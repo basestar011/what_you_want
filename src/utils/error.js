ae669f22393ea1a9e2fac472897b27acea26dab0
dongjun <basestar011@naver.com>
2022-05-16 18:07:25 +0900

    error handler 추가

---
+++ src/utils/error.js
@@ -0,0 +1,18 @@
+import router from '../router'
+
+export const handleError = (error) => {
+  console.dir(error);
+  if(error.response) {
+    const { status, data: { error: { name, message } } } = error.response;
+    if(status === 401) {
+      if(name === 'TokenExpiredError') {
+        alert('세션이 만료되었습니다.');
+        router.push('/');
+      } else {
+        return { name, message };
+      }
+    } else {
+      return { name, message };
+    }
+  }
+}
